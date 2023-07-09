4fbf074223ea19995fd46a7c4f191161393454db
Arthur <arturosardaryan@yandex.ru>
2023-07-10 01:02:52 +0300

    Initial commit

---
+++ app/src/main/java/ru/student/detected/composition/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.student.detected.composition
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
